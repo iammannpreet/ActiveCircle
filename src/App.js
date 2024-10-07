66fd57974a299ec543d7a6dfa557b3c779bb0dbf
Manpreet Singh <vickyarora.singh@gmail.com>
2024-10-06 23:55:27 -0400

    Update: Sun Oct6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
+      {/* Header */}
+      <Header />
 
+      {/* Banner */}
+      {/* Section */}
+      {/* Footer */}
     </div>
   );
+
 }
 
 export default App;
