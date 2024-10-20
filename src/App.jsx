9f4a246d151670fc1078d026f8cf7789cea9a4f0
Manpreet Singh <vickyarora.singh@gmail.com>
2024-10-20 13:27:11 -0400

    add: feature to add activity by geolocation manipulation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import './tailwind.css'
+import './tailwind.css';
 import Home from './pages/Home';
+import AddActivityPage from './pages/AddActivityPage'; // Import the new Add Activity page
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Router components
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div>
+        {/* Define Routes for your pages */}
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Home Page Route */}
+          <Route path="/add-activity" element={<AddActivityPage />} /> {/* Add Activity Page Route */}
+        </Routes>
+      </div>
+    </Router>
   );
-
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,51 +2,18 @@ import React from 'react';
 import Header from '../components/Header';
 import ParallaxSection from '../components/ParallaxSection';
 import HoverCard from '../components/HoverCard';
-import useActivities from '../hooks/useActivities';
 import Map from '../components/Map';
 import Hero from '../components/Hero';
 
 const Home = () => {
-    const { activities, newActivity, setNewActivity, handleAddActivity, handleDelete } = useActivities();
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setNewActivity({ ...newActivity, [name]: value });
-    };
-
     return (
         <>
             <Header />
             <Hero />
             <div className="home">
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    handleAddActivity(newActivity);
-                }}>
-                    <label>Type:</label>
-                    <input type="text" name="type" value={newActivity.type} onChange={handleInputChange} required />
-                    <label>Location:</label>
-                    <input type="text" name="location" value={newActivity.location} onChange={handleInputChange} required />
-                    <label>Details:</label>
-                    <input type="text" name="details" value={newActivity.details} onChange={handleInputChange} />
-                    <label>Organizer:</label>
-                    <input type="text" name="organizer" value={newActivity.organizer} onChange={handleInputChange} />
-                    <button type="submit">Add Activity</button>
-                </form>
-
-                <ul>
-                    {activities.map((activity) => (
-                        <li key={activity._id}>
-                            {activity.type} at {activity.location}
-                            <button onClick={() => handleDelete(activity._id)}>Delete</button>
-                        </li>
-                    ))}
-                </ul>
-
                 <ParallaxSection />
                 <HoverCard />
             </div>
-
         </>
     );
 };
